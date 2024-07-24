afb9e1cc60a1a0876bcba7118971b3ec97044a18
Kamich-GP <you@example.com>
2024-07-24 19:24:13 +0500

    urls,views

--- index/urls.py
+++ index/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home_page),
     path('product/<int:pk>', views.get_exact_pr),
-    path('category/<int:pk>', views.get_exact_category)
+    path('category/<int:pk>', views.get_exact_category),
+    path('add-to-cart/<int:pk>', views.to_cart),
+    path('del-from-cart/<int:pk>', views.del_from_cart),
+    path('cart', views.get_user_cart)
 ]
--- index/views.py
+++ index/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product, Category
+from django.shortcuts import render, redirect
+from .models import Product, Category, Cart
 
 
 # Create your views here.
@@ -29,3 +29,30 @@ def get_exact_category(request, pk):
     # Передаем данные на фронт
     context = {'products': products}
     return render(request, 'category.html', context)
+
+
+def to_cart(request, pk):
+    if request.method == 'POST':
+        product = Product.objects.filter(id=pk)
+        if product.pr_count >= int(request.POST.get('user_product_quantity')):
+            Cart.objects.create(user_id=request.user.id,
+                                user_product=product.pr_name,
+                                user_product_quantity=int(request.POST.get
+                                                          ('user_product_quantity'))).save()
+            return redirect('/')
+
+
+def get_user_cart(request):
+    user_cart = Cart.objects.filter(user_id=request.user.id)
+
+    # Отправляем данные на фронт
+    context = {'cart': user_cart}
+    return render(request, 'cart.html', context)
+
+
+def del_from_cart(request, pk):
+    product_to_delete = Product.objects.filter(id=pk)
+    Cart.objects.filter(user_id=request.user.id,
+                        user_product=product_to_delete).delete()
+
+    return redirect('/cart')
